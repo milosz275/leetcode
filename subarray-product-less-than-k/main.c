13bc31cfea9c35d7ae0d90cadecd9bbf930adfe8
mldxo <miloszmaculewicz@gmail.com>
2024-06-27 15:12:07 +0200

    Add subarray function

--- subarray-product-less-than-k/main.c
+++ subarray-product-less-than-k/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NUM_SIZE 4
+#define K 100
+
+int numSubarrayProductLessThanK(int* nums, int numsSize, int k)
+{
+    int subarrayCount = 0;
+
+
+
+
+    return subarrayCount;
+}
+
 int main()
 {
-
+    int nums[NUM_SIZE] = { 10, 5, 2, 6 };
+    int result = numSubarrayProductLessThanK(nums, NUM_SIZE, K);
+    printf("Subarray product less than k count: %d\n", result);
     exit(EXIT_SUCCESS);
 }
