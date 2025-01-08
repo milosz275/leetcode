ac6c7e4bc5882da9ee191f0a95168a59ac2e51ec
milosz275 <miloszmaculewicz@gmail.com>
2025-01-08 16:16:58 +0100

    Add Continuous Subarrays

---
+++ continuous-subarrays/main.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    long long continuousSubarrays(vector<int>& nums)
+    {
+        int n = (int)nums.size();
+        int left = 0;
+        long long count = 0;
+        deque<int> max_deque, min_deque;
+
+        for (int right = 0; right < n; ++right)
+        {
+            // Add the current element to the max deque
+            while (!max_deque.empty() && nums[max_deque.back()] < nums[right])
+                max_deque.pop_back();
+            max_deque.push_back(right);
+
+            // Add the current element to the min deque
+            while (!min_deque.empty() && nums[min_deque.back()] > nums[right])
+                min_deque.pop_back();
+            min_deque.push_back(right);
+
+            // Shrink the window if the condition is violated
+            while (nums[max_deque.front()] - nums[min_deque.front()] > 2)
+            {
+                ++left;
+                if (!max_deque.empty() && max_deque.front() < left)
+                    max_deque.pop_front();
+                if (!min_deque.empty() && min_deque.front() < left)
+                    min_deque.pop_front();
+            }
+
+            // Count valid subarrays ending at 'right'
+            count += (right - left + 1);
+        }
+        return count;
+    }
+};
+
+int main()
+{
+    vector<int> array = {5,4,2,4};
+    cout << Solution().continuousSubarrays(array) << endl;
+    return 0;
+}
