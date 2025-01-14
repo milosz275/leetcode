1830d135a17931b2d5da0525d0d1d6d7f13c1990
milosz275 <miloszmaculewicz@gmail.com>
2025-01-14 13:58:14 +0100

    Add First Letter to Appear Twice

---
+++ first-letter-to-appear-twice/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    char repeatedCharacter(string s)
+    {
+        set<char> characters;
+        for (char c: s)
+        {
+            if (characters.find(c) != characters.end())
+                return c;
+            else
+                characters.insert(c);
+        }
+        return '\0';
+    }
+};
