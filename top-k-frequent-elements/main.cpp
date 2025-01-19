9fe390e2e639c791c9c8517882ab94266a0b47fe
milosz275 <miloszmaculewicz@gmail.com>
2025-01-19 22:35:15 +0100

    Add Top K Frequent Elements

---
+++ top-k-frequent-elements/main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k)
+    {
+        unordered_map<int, int> occurrences; // (num, occurrences)
+        for (int el: nums)
+            occurrences[el]++;
+
+        auto comparator = [](pair<int, int>& A, pair<int, int>& B)
+        {
+            return A.second > B.second; // min-heap by frequency
+        };
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comparator)> min_heap(comparator);
+
+        for (const pair<int, int>& occ: occurrences)
+        {
+            min_heap.push(occ);
+            if (min_heap.size() > k)
+                min_heap.pop(); // remove the least frequent element if size exceeds k
+        }
+
+        vector<int> answer;
+        while (!min_heap.empty())
+        {
+            answer.push_back(min_heap.top().first);
+            min_heap.pop();
+        }
+        return answer;
+    }
+};
+
+
+int main()
+{
+
+    return 0;
+}
