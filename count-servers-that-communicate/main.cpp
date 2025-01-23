bf0553bfe62c49b076feffbd3a325196a1e505ef
milosz275 <miloszmaculewicz@gmail.com>
2025-01-23 20:30:16 +0100

    Add Count Servers That Communicate

---
+++ count-servers-that-communicate/main.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int countServers(vector<vector<int>>& grid)
+    {
+        vector<int> row_count(grid.size(), 0);
+        vector<int> col_count(grid[0].size(), 0);
+        for (int i = 0; i < (int)grid.size(); ++i)
+        {
+            for (int j = 0; j < (int)grid[0].size(); ++j)
+            {
+                if (grid[i][j] == 1)
+                {
+                    col_count[i]++;
+                    row_count[j]++;
+                }
+            }
+        }
+        int counter = 0;
+        for (int i = 0; i < (int)grid.size(); ++i)
+        {
+            for (int j = 0; j < (int)grid[0].size(); ++j)
+            {
+                if (grid[i][j] == 1 && (col_count[i] > 1 || row_count[j] > 1))
+                    counter++;
+            }
+        }
+        return counter;
+    }
+};
+
+int main()
+{
+    vector<vector<int>> grid = {{1,1,0,0}, {0,0,1,0}, {0,0,1,0}, {0,0,0,1}};
+    cout << Solution().countServers(grid) << '\n';
+    return 0;
+}
