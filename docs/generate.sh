b241e5a4d04154f2350a2550607527033728c223
milosz275 <miloszmaculewicz@gmail.com>
2025-06-06 12:18:03 +0200

    Add Doxygen and MkDocs support

--- docs/generate.sh
+++ docs/generate.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
+set -e
+
 parent_dir=$(dirname "$(dirname "$(readlink -f "$0")")")
+output_dir="$parent_dir/docs"
+
+find "$output_dir" -mindepth 1 -maxdepth 1 -type d -exec rm -rf {} +
 
 for dir in "$parent_dir"/*/; do
     dir_name=$(basename "$dir")
-    if [[ "$dir_name" != "doc" && "$dir_name" != ".vscode" && "$dir_name" != ".git" && "$dir_name" != ".notes" ]] && [ -f "$dir/Doxyfile" ];
-    then
-        cd "$dir" && doxygen
+
+    if [[ "$dir_name" =~ ^(docs|.vscode|.git|.notes)$ ]]; then
+        continue
+    fi
+
+    # C/C++
+    if [ -f "$dir/Doxyfile" ]; then
+        echo "Generating Doxygen for $dir_name"
+        (
+            cd "$dir"
+            doxygen
+        )
+    fi
+
+    # Python
+    if [ -f "$dir/mkdocs.yml" ]; then
+        echo "Generating MkDocs for $dir_name"
+        mkdocs build -f "$dir/mkdocs.yml" -d "$output_dir/$dir_name"
     fi
 done
